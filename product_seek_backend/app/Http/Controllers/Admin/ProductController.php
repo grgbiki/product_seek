33838e5fd1b4e6d516bc2519b98318089088dbda
Your Name <you@example.com>
2020-08-13 13:56:10 +0545

    product-category-and-stores

---
+++ product_seek_backend/app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+use Illuminate\Support\Str;
+
+class ProductController extends Controller
+
+{
+
+	// paginated products
+	public function paginated_products(){
+		return Product::latest()->with('productCategory','productStore')->paginate(10);
+	}
+
+	// end paginated products
+	//add product
+
+  public function store(Request $request){
+  	$this->validate($request,[
+  		'title'=>'required',
+  		'price'=>'required',
+  	]);
+
+  	$slug=Str::slug($request->title,'-');
+  	$images=$request->product_image;
+
+  	if($images){
+
+  		for($i=0; $i<count($images);$i++){
+  			$product_images[$i]=$this->makeStoreProductImage($images[$i],$slug,$i);
+  		}
+
+  		$request->merge(['product_image'=>serialize($product_images)]);
+
+  	}else{
+  		$request->merge(['product_image'=>'no-image.jpg']);
+  	}
+
+  	$product=Product::create([
+
+  		'title'=>$request['title'],
+  		'price'=>$request['price'],
+  		'product_image'=>$request['product_image'],
+  		'description'=>$request['description'],
+
+  	]);
+
+  	if($request->category_id){
+  		$product->productCategory()->sync($request->category_id);
+  	}
+
+  	if($request->store_id){
+  		$product->productStore()->sync($request->store_id);
+  	}
+
+  }
+  //add product
+
+  // make store product image function
+  public function makeStoreProductImage($image,$title,$index){
+  	if($image){
+			$imageName=$title.'-'.$index.'-'.time().'.'.explode('/',explode(':',substr($image,0,strpos($image, ';')))[1])[1];
+			$imageExt=explode('.',$imageName);
+
+			preg_match("/data:image\/(.*?);/",$image,$imageExt[1]); // extract the image extension
+			$image = preg_replace('/data:image\/(.*?);base64,/','',$image); // remove the type part
+			$image = str_replace(' ', '+', $image);
+
+			\File::put(public_path('/images/uploads/') . $imageName, base64_decode($image));
+		}else{
+			$imageName='no-image.jpg';
+		}
+
+		return $imageName;
+  }
+  // end make store product image function
+
+  //edit 
+  public function show($id){
+		$product =  Product::latest()->with('productCategory','productStore');
+		$current_product=$product->findOrFail($id);
+		if($current_product->product_image!='no-laptop-image.jpg'){
+      $current_product->product_image=unserialize($current_product->product_image);
+    }
+		return $current_product;
+	}
+	//edit
+}
---
+++ product_seek_backend/resources/js/components/backend/store/Store.vue
@@ -0,0 +1,238 @@
+<template>
+	<div class="row">
+	<loader v-if='loading'></loader>
+		<div class="col-md-12">
+			<div class="col-md-12">
+				<div class="row">
+					<button class="btn btn-primary mr-1" @click='openModel'>
+						<i class="fas fa-plus mr-1"></i>Create Store
+					</button>
+					<a :href="store_url+'/trashed'">
+						<button class="btn btn-danger ">
+							<i class="fas fa-trash mr-1"></i>Trash
+						</button>
+					</a>
+				</div>
+			</div>
+
+			<div class="card mt-2">
+				<div class="card-header">
+					<h4>Stores</h4>
+				</div>
+				<div class="card-bodytable-responsive p-0">
+					<table class="table table-hover text-nowrap">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>No of Products</th>
+                <th>Email</th>
+                <th>Contact</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            	<tr v-if='!storesLength'>
+            		<td colspan="5">Sorry! no data found</td>
+            	</tr>
+            	<tr v-else v-for='pc in stores.data' :key='pc.id'>
+            		<td>{{ pc.name }}</td>
+            		<td>0</td>
+            		<td>{{ pc.email }}</td>
+            		<td>{{ pc.contact }}</td>
+            		<td>
+            			<button class="btn btn-primary mr-2" @click='editStore(pc.id)'><i class="fas fa-edit mr-2"></i>Edit</button>
+            			<button class="btn btn-danger" @click='trashStore(pc.id)'><i class="fas fa-trash mr-2"></i>Trash</button>
+            		</td>
+            	</tr>
+            </tbody>
+             <tfoot>
+		            <tr>
+                	<th>Name</th>
+	                <th>No of Products</th>
+	                <th>Email</th>
+	                <th>Contact</th>
+	                <th>Action</th>
+	              </tr>
+		          </tfoot>
+          </table>
+				</div>
+				<div class="card-footer">
+					<pagination :data="stores" @pagination-change-page="getResults"></pagination>
+				</div>
+			</div>
+
+			<div class="modal fade" id="storeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			  <div class="modal-dialog modal-dialog-centered" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			      	<h5 class="modal-title" v-if='editmode'id="exampleModalLongTitle">Edit Store</h5>
+			        <h5 class="modal-title" v-else id="exampleModalLongTitle">Create Store</h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+			      <form @submit.prevent="editmode? updateStore() : createStore()">
+				      <div class="modal-body">
+				        <div class="form-group">
+				        	<label for="name">Store's name</label>
+			        		<input v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }" placeholder="Store's Name">
+					      <has-error :form="form" field="name"></has-error>
+				        </div>
+				         <div class="form-group">
+				        	<label for="email">Email</label>
+			        		<input v-model="form.email" type="email" name="email" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" placeholder="Email">
+					      <has-error :form="form" field="name"></has-error>
+				        </div>
+			          <div class="form-group">
+				        	<label for="contact">Contact</label>
+			        		<input v-model="form.contact" type="tel" name="contact" class="form-control" :class="{ 'is-invalid': form.errors.has('contact') }" placeholder="Contact">
+					      <has-error :form="form" field="contact"></has-error>
+				        </div>
+				        <div class="form-group">
+				        	<label for="address">Address</label>
+			        		<input v-model="form.address" type="text" name="address" class="form-control" :class="{ 'is-invalid': form.errors.has('address') }" placeholder="Address">
+					      <has-error :form="form" field="contact"></has-error>
+				        </div>
+				         <div class="form-group">
+				        	<label for="google_maps_url">Google Map Url</label>
+			        		<input v-model="form.google_maps_url" type="url" name="google_maps_url" class="form-control" :class="{ 'is-invalid': form.errors.has('google_maps_url') }" placeholder="Google Map Url">
+					      <has-error :form="form" field="google_maps_url"></has-error>
+				        </div>
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancle</button>
+				        <button type="submit" class="btn btn-primary" v-if='editmode'>Update</button>
+				        <button type="submit" class="btn btn-primary" v-else>Create</button>
+				      </div>
+				    </form>
+			    </div>
+			  </div>
+			</div>
+
+		</div>
+		
+	</div>
+</template>
+<script>
+	export default{
+		name:'store',
+		props:['store_url'],
+		data(){
+			return{
+
+				form:new Form({
+					id:'',
+					name:'',
+					email:'',
+					contact:'',
+					address:'',
+					google_maps_url:'',
+				}),
+
+				stores:{},
+				storesLength:null,
+				editmode:false,
+				loading:true,
+				
+			}
+		},
+		methods:{
+			openModel(){
+				this.editmode=false
+				this.form.clear()
+				this.form.reset()
+				$('#storeModal').modal('show')
+			},
+			createStore(){
+				this.loading=true
+				this.form.post(this.store_url+'/store/').then(()=>{
+
+					Toast.fire({
+            icon: 'success',
+            title: 'Product\'s Category Created successfully'
+          })
+          this.loadStore()
+					$('#storeModal').modal('hide')
+          this.loading=false;
+
+				}).catch((response)=>{
+					if(response.message=='Request failed with status code 401'){
+						location.reload()
+					}
+					this.loading=false
+				});
+			},
+
+			getResults(page = 1) {
+        this.loading=true;
+        axios.get(this.store_url+'/cat-paginated/?page=' + page)
+        .then(response => {
+            this.stores = response.data;
+            this.loading=false;
+        });
+      },
+
+			loadStore(){
+				axios.get(this.store_url+'/store-paginated/').then(({data})=>{
+					this.stores=data
+					this.storesLength=this.stores.data.length
+					this.loading=false
+				})
+			},
+
+			editStore(id){
+				this.loading=true
+				this.editmode=true
+				let vm=this;
+				axios.get(this.store_url+'/show/'+id).then(function(response){
+					vm.form.fill(response.data)
+					$('#storeModal').modal('show')
+					vm.loading=false
+				})
+			},
+
+			updateStore(){
+				this.loading=true
+				this.form.put(this.store_url+'/update/'+this.form.id).then(()=>{
+
+					Toast.fire({
+            icon: 'success',
+            title: 'Product\'s Category updated successfully'
+          })
+          this.loadStore()
+					$('#storeModal').modal('hide')
+          this.loading=false;
+
+				}).catch((response)=>{
+					if(response.message=='Request failed with status code 401'){
+						location.reload()
+					}
+					this.loading=false
+				});
+			},
+
+			trashStore(id){
+				this.loading=true
+				this.form.get(this.store_url+'/trash/'+id).then(()=>{
+
+					Toast.fire({
+            icon: 'success',
+            title: 'Product\'s Category moved to trash Successfully'
+          })
+          this.loadStore()
+					$('#storeModal').modal('hide')
+          this.loading=false;
+
+				}).catch((response)=>{
+					if(response.message=='Request failed with status code 401'){
+						location.reload()
+					}
+					this.loading=false
+				});
+			},
+		},
+		created(){
+			this.loadStore()
+		}
+	}
+</script>
