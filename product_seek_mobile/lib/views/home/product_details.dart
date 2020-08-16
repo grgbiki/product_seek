fa627d73f2490515fd2c80aa0ea245658f350cc1
grgbiki <grgbiki53@gmail.com>
2020-08-16 20:42:01 +0545

    bug fix

--- product_seek_mobile/lib/views/home/product_details.dart
+++ product_seek_mobile/lib/views/home/product_details.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:after_layout/after_layout.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
